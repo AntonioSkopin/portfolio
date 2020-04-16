765aa18d9113eae6238b11a1c250718278ecd45c
Antonio Skopin <askopin@outlook.com>
2020-04-16 10:59:06 +0200

    Updates

---
+++ precache-manifest.a21c93b18049a194ef6f40fe0c62368d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba56787a06dc7538d02bb2a16644c72",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2f21802af2cae9a08dfa",
+    "url": "/portfolio/static/css/main.f3da3fec.chunk.css"
+  },
+  {
+    "revision": "bf0bb3dd508ed7f72867",
+    "url": "/portfolio/static/js/2.3a598701.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.3a598701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f21802af2cae9a08dfa",
+    "url": "/portfolio/static/js/main.324e0187.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "23be83b9b4a96e25715c0c99c3310f00",
+    "url": "/portfolio/static/media/Calculator-project.23be83b9.png"
+  },
+  {
+    "revision": "0925416b3df38eb23f1d006edf65d7d8",
+    "url": "/portfolio/static/media/info.0925416b.png"
+  },
+  {
+    "revision": "5bf6011780032bb1bb97014ed24cb5a9",
+    "url": "/portfolio/static/media/instagram (original).5bf60117.png"
+  },
+  {
+    "revision": "cdb616fe3f39d2ef1fcf19b9f907c221",
+    "url": "/portfolio/static/media/instagram.cdb616fe.png"
+  },
+  {
+    "revision": "b86527bb56d338838b3e25884b53f04d",
+    "url": "/portfolio/static/media/phone.b86527bb.png"
+  },
+  {
+    "revision": "e6f3fac675b17e506fdbe1ab3273a634",
+    "url": "/portfolio/static/media/project.e6f3fac6.png"
+  },
+  {
+    "revision": "ed06e3b25b6da23b8e2d0a94dc93e32d",
+    "url": "/portfolio/static/media/tijdelijk.ed06e3b2.JPG"
+  },
+  {
+    "revision": "86f250d83c792c749ad545a07f1292d7",
+    "url": "/portfolio/static/media/under_construction.86f250d8.jpg"
+  },
+  {
+    "revision": "746b58dee481ce9036adf34a88ad626a",
+    "url": "/portfolio/static/media/user (white).746b58de.png"
+  },
+  {
+    "revision": "4448fba36cdff09b44d8efd43c7a8a17",
+    "url": "/portfolio/static/media/user.4448fba3.png"
+  }
+]);
